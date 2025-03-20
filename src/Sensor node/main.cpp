3a9531ca698ee82700e1d089bbbeb9652ee86a09
Mikhaerys <mikhaerys@gmail.com>
2025-03-19 22:22:16 -0500

    Add DHT sensor support to Sensor node

--- src/Sensor node/main.cpp
+++ src/Sensor node/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define DHTTYPE DHT11
+#define DHTPIN 4
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
+    Serial.begin(115200);
+    dht.begin();
 }
 
 void loop()
 {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t))
+    {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
 }
